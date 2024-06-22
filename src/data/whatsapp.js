d2c6d97bac6adc62654a49d8a73b0197e2776dd7
PRABODH1243 <prabodhbadimi1@gmail.com>
2024-06-22 21:41:39 +0530

    hello

---
+++ src/data/whatsapp.js
@@ -0,0 +1,66 @@
+import {
+  cs1,
+} from "../assets/whatsapp";
+
+export const chatsData = [
+  {
+    pp: cs1,
+    contact: "Rahul",
+    msg: "Awesome, thank you!",
+    time: "1:15 pm",
+    unreadMsgs: null,
+  },
+];
+
+export const messagesData = [
+  {
+    msg: "Hey ",
+    time: "10:58 am",
+    sent: true,
+  },
+  {
+    msg: "Hi",
+    time: "10:59 am",
+    sent: false,
+  },
+  {
+    msg: "How are you?",
+    time: "10:59 am",
+    sent: true,
+  },
+  {
+    msg: "Am fine, What about you? ",
+    time: "11:00 am",
+    sent: false,
+  },
+  {
+    msg: "I am also fine",
+    time: "11:01 am",
+    sent: true,
+  },
+  {
+    msg: "Where are you? Lets meet?",
+    time: "11:01 am",
+    sent: true,
+  },
+  {
+    msg: "I am at home, Yaa sure!",
+    time: "11:02 am",
+    sent: false,
+  },
+  {
+    msg: "Lets cautch up in ITM resturnt at 2pm",
+    time: "11:02 am",
+    sent: true,
+  },
+  {
+    msg: "Yaa sure!",
+    time: "11:28 am",
+    sent: false,
+  },
+  {
+    msg: "Cool",
+    time: "11:36 am",
+    sent: true,
+  },
+];
